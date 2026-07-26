13b7a34479f7c6209837e15e20ba9c5a2d94c4be
Mario Nady <marionadydev@gmail.com>
2026-07-26 21:00:36 +0300

    fix some bugs, add some design

---
+++ lib/core/theme/app_sizes.dart
@@ -0,0 +1,9 @@
+abstract class AppSizes {
+  static const double s4 = 4.0;
+  static const double s8 = 8.0;
+  static const double s12 = 12.0;
+  static const double s16 = 16.0;
+  static const double s20 = 20.0;
+  static const double s32 = 32.0;
+  static const double s64 = 64.0;
+}
