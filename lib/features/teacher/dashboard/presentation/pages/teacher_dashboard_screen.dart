337a74d5b70fd0a47ddfbfb3fa3192c11736687d
Mario Nady <marionadydev@gmail.com>
2026-08-05 16:05:01 +0300

    improve teacher dashboard

--- lib/features/teacher/dashboard/presentation/pages/teacher_dashboard_screen.dart
+++ lib/features/teacher/dashboard/presentation/pages/teacher_dashboard_screen.dart
@@ -1,11 +1,9 @@
 import 'package:draya_mobile/core/helpers/app_extensions.dart';
-import 'package:draya_mobile/core/theme/app_colors.dart';
 import 'package:draya_mobile/core/theme/app_sizes.dart';
 import 'package:draya_mobile/core/view_models/drawer_model.dart';
 import 'package:draya_mobile/core/widgets/app_card_container_empty.dart';
 import 'package:draya_mobile/core/widgets/app_card_container_info.dart';
 import 'package:draya_mobile/core/widgets/app_drawer.dart';
-import 'package:draya_mobile/core/widgets/app_elevated_button.dart';
 import 'package:draya_mobile/core/widgets/app_list_tile_info.dart';
 import 'package:draya_mobile/core/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
@@ -88,65 +86,53 @@ class TeacherDashboardScreen extends StatelessWidget {
                 ],
               ),
               const SizedBox(height: AppSizes.s20),
-              Container(
-                padding: const EdgeInsets.all(AppSizes.s20),
-                decoration: BoxDecoration(
-                  color: AppColors.primary50,
-                  borderRadius: BorderRadius.circular(AppSizes.s20),
-                  border: Border.all(
-                    color: AppColors.primary200,
-                    width: AppSizes.s1,
+              AppCardContainerEmpty(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "طلاب يحتاجون متابعة",
+                            style: context.textTheme.labelLarge,
+                          ),
+                          const Text("اداء دون 55% فى اخر امتحان"),
+                        ],
+                      ),
+                      TextButton.icon(
+                        onPressed: () {},
+                        label: const Text("الكل"),
+                        icon: const Icon(Icons.keyboard_arrow_left_rounded),
+                        iconAlignment: IconAlignment.end,
+                      ),
+                    ],
                   ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              "طلاب يحتاجون متابعة",
-                              style: context.textTheme.labelLarge,
-                            ),
-                            const Text("اداء دون 55% فى اخر امتحان"),
-                          ],
-                        ),
-                        TextButton.icon(
-                          onPressed: () {},
-                          label: const Text("الكل"),
-                          icon: const Icon(Icons.keyboard_arrow_left_rounded),
-                          iconAlignment: IconAlignment.end,
-                        ),
-                      ],
-                    ),
-                    const Divider(),
-                    const Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text("الطالب"),
-                        Text("الكورس"),
-                        Text("الدرجة"),
-                      ],
-                    ),
-                    ...studentsNeedHelp.map(
-                      (student) => Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: AppSizes.s8,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text(student.$1),
-                            Text(student.$2),
-                            Text("${student.$3.toInt()}%"),
-                          ],
-                        ),
+                  const Divider(),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text("الطالب"),
+                      Text("الكورس"),
+                      Text("الدرجة"),
+                    ],
+                  ),
+                  ...studentsNeedHelp.map(
+                    (student) => Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: AppSizes.s8,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(student.$1),
+                          Text(student.$2),
+                          Text("${student.$3.toInt()}%"),
+                        ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ],
           ),
